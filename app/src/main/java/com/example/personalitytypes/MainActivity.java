fa9615609e695b3a09fcc810783083e5c7080b91
Tengku Romansyah <tengku.romansyah@gmail.com>
2020-01-15 21:39:31 +0700

    Style updated

--- app/src/main/java/com/example/personalitytypes/MainActivity.java
+++ app/src/main/java/com/example/personalitytypes/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.personalitytypes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_main);
+
+        // define about button
+        Button btnAbout = findViewById(R.id.btn_about);
+        btnAbout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_about:
+                Intent moveToAbout = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(moveToAbout);
+                break;
+        }
     }
 }
